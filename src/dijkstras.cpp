1cbd7a38e50d59d0cb2a646cb4ef5099f1071220
L <xingyl26@uci.edu>
2025-03-12 03:02:58 +0000

    dijkstras pass small

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,4 +1,4 @@
-#include "dijkstra.h"
+#include "dijkstras.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -8,9 +8,54 @@
 using namespace std;
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
-    
+    vector<int> G_distance(G.numVertices, INF);
+    G_distance[source] = 0;
+    previous[source] = source;
+    vector<bool> visited(G.numVertices, false);
+    visited[source] = true;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({source, 0});
+
+    while (!pq.empty()){
+        auto [v, v_weight] = pq.top();
+        pq.pop();
+        //vector<Edge> connections = edge_from(G, v);
+        for (Edge e: G[v]){
+            int new_distance = e.weight + v_weight;
+            if (!visited[e.dst] &&  new_distance< G_distance[e.dst]){
+                visited[e.dst] = true;
+                G_distance[e.dst] = new_distance;
+                pq.push({e.dst, new_distance});
+                previous[e.dst] = v;
+            }
+        }
+    }
+    return G_distance;
 }
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
+
+void insert_path(vector<int>& v, int new_replacement){
+    if (v.empty()) {
+        v.push_back(new_replacement);
+        return;
+    }
+
+    // Move elements to the right
+    v.insert(v.begin(), new_replacement);
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+    vector<int> path;
+    //path.push_back(destination);
+    int current_v = destination;
+    
+    while(previous[current_v] != current_v){
+        cout << current_v;
+        insert_path(path, current_v);
+        current_v = previous[current_v];
+    }
+    insert_path(path, current_v);
+    return path;
+}
 
 void print_path(const vector<int>& v, int total){
     cout << "Total cost is " << total << endl; 
--- src/dijkstras.h
+++ src/dijkstras.h
@@ -47,6 +47,16 @@ inline void file_to_graph(const string& filename, Graph& G) {
     in.close();
 }
 
+// vector<Edge> edge_from(const Graph G, Edge source){
+//     vector<Edge> result;
+//     for (Edge &e: G){
+//         if (e.src == source){
+//             result.push_back(e);
+//         }
+//     }
+//     return result;
+// }
+void move_down(vector<int> & v, int new_replacement);
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous);
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
 void print_path(const vector<int>& v, int total);
